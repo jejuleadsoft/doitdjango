4af358fdb5c9f4fc3ce94b27c5ea54ac09d0cd5b
jejuleadsoft <kjoa99@gmail.com>
2021-08-02 18:23:24 +0900

    포스트 목로 페이지에 카테고리 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
 #    objects = None
     title = models.CharField(max_length=30)
@@ -16,7 +26,8 @@ class Post(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
