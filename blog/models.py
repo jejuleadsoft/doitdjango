3ec285981c26c2004c4032804b21e185905ae54c
jejuleadsoft <kjoa99@gmail.com>
2021-08-02 13:04:57 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 class Post(models.Model):
+#    objects = None
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d',blank=True)
@@ -13,8 +16,10 @@ class Post(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
